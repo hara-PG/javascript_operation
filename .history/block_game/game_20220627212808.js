67496bf9666cee9cf1b537ad2ff2400332e13985
hara-PG <hara.job2021@gmail.com>
2022-06-27 21:30:10 +0900

    ボールが消えるようにする

--- .history/block_game/game_20220627212808.js
+++ .history/block_game/game_20220627212808.js
@@ -81,17 +81,15 @@ for (let c = 0; c < brickColumnCount; c++) {
 function drawBricks() {
   for (let c = 0; c < brickColumnCount; c++) {
     for (let r = 0; r < brickRowCount; r++) {
-      if (bricks[c][r].status == 1) {
-        let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-        let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = color;
-        ctx.fill();
-        ctx.closePath();
-      }
+      let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+      let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = color;
+      ctx.fill();
+      ctx.closePath();
     }
   }
 }
